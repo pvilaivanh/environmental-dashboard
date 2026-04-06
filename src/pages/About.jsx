278d669b3e1e03aa54665a21290c6256e631e60b
Punoi Vilaivanh <pvilaivanh@gmail.com>
2026-04-06 09:42:58 -0400

    Included partner's name

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ function About() {
       <h3>Punoi Vilaivanh</h3>
       <p>Worked on frontend and UI design.</p>
 
-      <h3>Partner Name</h3>
+      <h3>Jacob Hammonds</h3>
       <p>Worked on backend and API integration.</p>
     </div>
   );
