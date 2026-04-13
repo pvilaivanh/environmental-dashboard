7e91a32cdfee1dbcaa54166c29d187640b6814d1
Punoi Vilaivanh <pvilaivanh@gmail.com>
2026-04-13 16:12:59 -0400

    added login and register pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Forecast from "./pages/Forecast";
 import About from "./pages/About";
 import "./App.css";
 import { useState } from "react";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -14,40 +16,64 @@ function App() {
     setDarkMode(!darkMode);
   };
 
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user"))
+  )
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    setUser(null);
+  }
 
   return (
-    <Router>
-      <div className={`app ${darkMode ? "dark-mode" : ""}`}>
-        
-        {/* Sidebar */}
-        <nav className="sidebar">
-          <h1>ENVIROMonitor 1000</h1>
-          <hr style={{ width: "100%" }}/>
-          <Link to="/">Home</Link>
-          <Link to="/indoor">Indoor</Link>
-          <Link to="/outdoor">Outdoor</Link>
-          <Link to="/forecast">Forecast</Link>
-          <Link to="/about">About</Link>
-
-          <button className="toggle-button" onClick={toggleDarkMode}>
-            {darkMode ? "Light Mode" : "Dark Mode"}
-          </button>
-        </nav>
-        
-
-        {/* Main Content */}
-        <div className="main-content">
+    <>
+      {!user ? (
+        <Router>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/indoor" element={<IndoorHistory />} />
-            <Route path="/outdoor" element={<OutdoorHistory />} />
-            <Route path="/forecast" element={<Forecast />} />
-            <Route path="/about" element={<About />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Login setUser={setUser} />} />
           </Routes>
-        </div>
+        </Router>
+      ) : (
+        <Router>
+          <div className={`app ${darkMode ? "dark-mode" : ""}`}>
+            
+            {/* Sidebar */}
+            <nav className="sidebar">
+              <h1>ENVIROMonitor 1000</h1>
+              <hr style={{ width: "100%" }}/>
+              <Link to="/">Home</Link>
+              <Link to="/indoor">Indoor</Link>
+              <Link to="/outdoor">Outdoor</Link>
+              <Link to="/forecast">Forecast</Link>
+              <Link to="/about">About</Link>
 
-      </div>
-    </Router>
+              <button className="toggle-button" onClick={toggleDarkMode}>
+                {darkMode ? "Light Mode" : "Dark Mode"}
+              </button>
+              <button className="logout-button" onClick={handleLogout}>
+                Logout
+              </button>
+
+            </nav>
+            
+
+            {/* Main Content */}
+            <div className="main-content">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/indoor" element={<IndoorHistory />} />
+                <Route path="/outdoor" element={<OutdoorHistory />} />
+                <Route path="/forecast" element={<Forecast />} />
+                <Route path="/about" element={<About />} />
+              </Routes>
+            </div>
+
+          </div>
+        </Router>
+      )}
+    </>
+
   );
 }
 
